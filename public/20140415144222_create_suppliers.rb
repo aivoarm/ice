a88847c358500262c04269345ddf675e348e7498
aivoarm <arman@ayvazyan.com>
2014-04-16 20:40:55 -0400

    add

---
+++ public/20140415144222_create_suppliers.rb
@@ -0,0 +1,29 @@
+class CreateSuppliers < ActiveRecord::Migration
+  def change
+    create_table :suppliers do |t|
+      t.string :SupplierNo
+      t.string :SupplerName
+      t.string :Account
+      t.string :SubAccount
+      t.string :OU
+      t.string :AB
+      t.string :BC
+      t.string :MA
+      t.string :NB
+      t.string :NF
+      t.string :NS
+      t.string :NU
+      t.string :NT
+      t.string :FC
+      t.string :ONT
+      t.string :PE
+      t.string :QC
+      t.string :SK
+      t.string :YU
+      t.string :IO
+      t.string :IQ
+
+      t.timestamps
+    end
+  end
+end
