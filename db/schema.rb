71a9e1f8cebd13c93212e0256f8b562200470dcc
aivoarm <arman@ayvazyan.com>
2014-04-16 21:16:13 -0400

    add

--- db/schema.rb
+++ db/schema.rb
@@ -13,5 +13,59 @@
 
 ActiveRecord::Schema.define(version: 20140415155913) do
 
-  
+  create_table "data_files", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "layouts", force: true do |t|
+    t.string   "description"
+    t.integer  "start"
+    t.integer  "length"
+    t.string   "ftype"
+    t.string   "ou"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "suppliers", force: true do |t|
+    t.string   "SupplierNo"
+    t.string   "SupplerName"
+    t.string   "Account"
+    t.string   "SubAccount"
+    t.string   "OU"
+    t.string   "AB"
+    t.string   "BC"
+    t.string   "MA"
+    t.string   "NB"
+    t.string   "NF"
+    t.string   "NS"
+    t.string   "NU"
+    t.string   "NT"
+    t.string   "FC"
+    t.string   "ONT"
+    t.string   "PE"
+    t.string   "QC"
+    t.string   "SK"
+    t.string   "YU"
+    t.string   "IO"
+    t.string   "IQ"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "uploads", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "filepath"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "user_id"
+    t.string   "password"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
