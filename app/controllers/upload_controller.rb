c5d54b193092a75bfc145ed38510f45139820f8d
aivoarm <arman@ayvazyan.com>
2014-04-20 13:59:52 -0400

    add

--- app/controllers/upload_controller.rb
+++
@@ -1,9 +0,0 @@
-class UploadController < ApplicationController
-  def index
-      render :file => 'uploadfile.html.erb'
-  end
-  def uploadFile
-      post = DataFile.save(params[:upload])
-      render :text => "File has been uploaded successfully"
-  end
-end
