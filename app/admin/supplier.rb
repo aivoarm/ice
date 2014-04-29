48baafec7ce9773042ab91bdc67e7a71e113a461
aivoarm <arman@ayvazyan.com>
2014-04-29 12:04:31 -0400

    aaa

--- app/admin/supplier.rb
+++ app/admin/supplier.rb
@@ -1,10 +1,10 @@
 ActiveAdmin.register Supplier do
 
-  
+  permit_params
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+   #permit_params :list, :of, :attributes, :on, :model
   #
   # or
   #
